44959f970c6ca54d64422d6ed1ffce4901b4eb70
avishisht-gupta-pw <avishisht.gupta@pw.live>
2024-12-01 15:13:03 +0530

    initiial  commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,19 @@ plugins {
     // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.composeMultiplatform) apply false
-    alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+//    alias(libs.plugins.google.playServices) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.kotlinCocoapods) apply false
+}
+
+apply<MyPlugin>()
+
+class MyPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        println("Avishisht's Plugin Applied")
+    }
 }
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,8 +5,14 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.serialization)
+//    alias(libs.plugins.google.playServices)
+    alias(libs.plugins.firebase.crashlytics)
+//    alias(libs.plugins.kotlinCocoapods)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 kotlin {
@@ -16,7 +22,9 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
+    jvm("desktop")
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -27,22 +35,113 @@ kotlin {
             isStatic = true
         }
     }
-    
+
+//    ios {
+//        binaries {
+//            framework {
+//                baseName = "ComposeApp"
+//            }
+//        }
+//    }
+
+//    cocoapods {
+//        summary = "Description of the ComposeApp shared module"
+//        homepage = "https://homepage.link" // Replace with your module's homepage
+//        ios.deploymentTarget = "14.1"
+//        podfile = project.file("../iosApp/Podfile")
+//
+//        framework {
+//            baseName = "ComposeApp"
+//        }
+//    }
+    sourceSets.commonMain {
+        kotlin.srcDir("build/generated/ksp/metadata")
+    }
     sourceSets {
-        
+        val desktopMain by getting
+
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
+            implementation(libs.ktor.client.okhttp)
+            implementation(libs.core.splashscreen)
+            implementation(libs.androidx.material3.android)  // Use Material3
+//            implementation(libs.accompanist.placeholder.material)
+            implementation(libs.accompanist.swiperefresh)
+            implementation(libs.accompanist.placeholder.material)
+            implementation(libs.accompanist.webview)
+            implementation(libs.lottie.compose.android)
+            implementation(libs.androidx.material3)
+
+            implementation(libs.androidx.paging.compose.v330)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
+
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)  // Replace material with material3
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            api(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.navigation.compose)
+
+            implementation(libs.bundles.ktor)
+
+            api(libs.datastore.preferences)
+            api(libs.datastore)
+
+            api(libs.moko.permissions)
+            api(libs.moko.permissions.compose)
+
+            implementation(libs.kmpauth.google) //Google One Tap Sign-In
+//            implementation(libs.kmpauth.firebase) //Integrated Authentications with Firebase
+            implementation(libs.kmpauth.uihelper) //UiHelper SignIn buttons (AppleSignIn, GoogleSignInButton)
+
+            // Coil
+            implementation(libs.coil.compose.core)
+            implementation(libs.coil.compose)
+            implementation(libs.coil)
+            implementation(libs.coil.network.ktor)
+
+            // Room
+            implementation(libs.room.runtime)
+            implementation(libs.sqlite.bundled)
+            implementation(libs.androidx.room.paging)
+
+            // Back Handling
+            implementation(libs.foundation)
+            implementation(libs.decompose)
+            implementation(libs.decompose.extension)
+            implementation(libs.paging.common)
+            implementation(libs.paging.common.compose)
+
+            implementation(libs.kotlinx.datetime)
+
+        }
+        nativeMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+            implementation(libs.accompanist.swiperefresh)
+            implementation(libs.accompanist.placeholder.material)
+            implementation(libs.accompanist.webview)
+//            implementation(libs.androidx.paging.common.jvm)
+//            implementation("app.cash.paging:paging-runtime-uikit:3.3.0-alpha02-0.5.1")
+        }
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutines.swing)
+            implementation(libs.oshi.core)
+
+            implementation(libs.ktor.client.okhttp)
+            implementation(libs.lottie.compose.desktop)
         }
     }
 }
@@ -50,6 +149,10 @@ kotlin {
 android {
     namespace = "com.falcon.split"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
         applicationId = "com.falcon.split"
@@ -72,9 +175,36 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        compose = true
+    }
+    dependencies {
+        debugImplementation(compose.uiTooling)
+    }
+}
+dependencies {
+    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.material3)
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.falcon.split.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "com.split.news"
+            packageVersion = "1.0.0"
+        }
+    }
+}
+
+
+room {
+    schemaDirectory("$projectDir/schemas")
 }
 
 dependencies {
-    debugImplementation(compose.uiTooling)
+//    implementation(libs.androidx.material3.jvmstubs)
+    ksp(libs.room.compiler)
 }
-
