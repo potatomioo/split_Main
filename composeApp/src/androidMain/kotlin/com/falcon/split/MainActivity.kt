c8fe9970754ce2afa216a57db11a9c0d08723d80
avishisht-gupta-pw <avishisht.gupta@pw.live>
2025-02-05 20:19:36 +0530

    done all changes, now going for testing

--- composeApp/src/androidMain/kotlin/com/falcon/split/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/falcon/split/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.falcon.split
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -23,14 +22,36 @@ import com.falcon.split.data.network.ApiClient
 import com.falcon.split.data.network.createHttpClient
 import com.falcon.split.presentation.profile.ProfileScreen
 import com.falcon.split.presentation.sign_in.GoogleAuthUiClient
-import com.falcon.split.presentation.sign_in.SignInScreen
 import com.falcon.split.presentation.sign_in.SignInViewModel
 import com.falcon.split.presentation.sign_in.UserState
 import com.falcon.split.screens.mainNavigation.OpenUpiApp
 import com.google.android.gms.auth.api.identity.Identity
 import io.ktor.client.engine.okhttp.OkHttp
 import kotlinx.coroutines.launch
-
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.falcon.split.LottieAnimationSpec
+import com.falcon.split.LottieAnimationView
+import com.falcon.split.SignInProgressPopup
+import com.falcon.split.saveUser
+import com.mmk.kmpauth.uihelper.google.GoogleSignInButton
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val googleAuthUiClient by lazy {
@@ -41,7 +62,6 @@ class MainActivity : ComponentActivity() {
     }
     private lateinit var contactManager : AndroidContactManager
 
-    @SuppressLint("ServiceCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         contactManager = AndroidContactManager(this)
@@ -182,7 +202,67 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+fun SignInScreen(
+    state: UserState,
+    viewModel: SignInViewModel,
+    navControllerCommon: NavHostController,
+    onSignInClick: () -> Unit
+) {
+    val context = LocalContext.current
+    LaunchedEffect(key1 = state) {
+        if (state is UserState.Error) {
+            Toast.makeText(
+                context,
+                state.error,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
 
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LottieAnimationView(LottieAnimationSpec("login_animation.json"))
+        Spacer(
+            modifier = Modifier.height(60.dp)
+        )
+        GoogleSignInButton(
+            onClick = {
+                onSignInClick()
+            }
+        )
+        Spacer(
+            modifier = Modifier.height(35.dp)
+        )
+    }
+
+    val userState by viewModel.userDetails.collectAsState()
+    when (userState) {
+        is UserState.Error -> {
+            val error = (userState as UserState.Error).error
+            println("ERROR_TAG$error")
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+            ) {
+                Text(
+                    text = "Error loading user: $error",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        UserState.Loading -> {
+            SignInProgressPopup()
+        }
+        is UserState.Success -> {
+            navControllerCommon.navigate("app_content")
+        }
+    }
+}
 
 //Contact Handling
 
