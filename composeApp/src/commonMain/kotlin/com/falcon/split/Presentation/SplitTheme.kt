4ea357801676a961bd14dfc6bec0a05cf3e15948
Deeptanshu Shukla <deeptanshushuklaji@gmail.com>
2025-02-08 20:21:48 +0530

    Added FontFam and AppTheme

---
+++ composeApp/src/commonMain/kotlin/com/falcon/split/Presentation/SplitTheme.kt
@@ -0,0 +1,48 @@
+package com.falcon.split.Presentation
+
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun getAppTypography(): Typography {
+    return Typography(
+        bodyLarge = TextStyle(
+            fontFamily = AppFontFamily.nunitoFamily(),
+            fontWeight = FontWeight.Normal,
+            fontSize = 18.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.5.sp
+        ),
+        bodyMedium = TextStyle(
+            fontFamily = AppFontFamily.nunitoFamily(),
+            fontWeight = FontWeight.Normal,
+            fontSize = 15.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.5.sp
+        ),
+        titleLarge = TextStyle(
+            fontFamily = AppFontFamily.nunitoFamily(),
+            fontWeight = FontWeight.Bold,
+            fontSize = 17.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 0.sp
+        ),
+        titleSmall = TextStyle(
+            fontFamily = AppFontFamily.nunitoFamily(),
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 14.sp,
+            lineHeight = 25.sp,
+            letterSpacing = 0.sp
+        ),
+        titleMedium = TextStyle(
+            fontFamily = AppFontFamily.nunitoFamily(),
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 16.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 0.sp
+        )
+    )
+}
