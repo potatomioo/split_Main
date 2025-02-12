232082f77a8ccf4db2a2361a923b8464f86785e8
Deeptanshu Shukla <deeptanshushuklaji@gmail.com>
2025-02-12 18:40:04 +0530

    AddExpenseUseCase

---
+++ composeApp/src/commonMain/kotlin/com/falcon/split/domain/useCase/expense/AddExpenseUseCase.kt
@@ -0,0 +1,11 @@
+package com.falcon.split.domain.useCase.expense
+
+import com.falcon.split.data.Repository.ExpenseRepository
+import com.falcon.split.data.network.models_app.Expense
+
+class AddExpenseUseCase(
+    private val expenseRepository: ExpenseRepository
+) {
+    suspend operator fun invoke(groupId: String, expense: Expense): Result<Unit> =
+        expenseRepository.addExpense(groupId, expense)
+}
