29d587bed18336dd28b1f9e4845a9e6145076ea8
Deeptanshu Shukla <deeptanshushuklaji@gmail.com>
2025-02-16 20:17:40 +0530

    Updated addExpense function

--- composeApp/src/commonMain/kotlin/com/falcon/split/data/Repository/ExpenseRepository.kt
+++ composeApp/src/commonMain/kotlin/com/falcon/split/data/Repository/ExpenseRepository.kt
@@ -4,7 +4,7 @@ import com.falcon.split.data.network.models_app.Expense
 import kotlinx.coroutines.flow.Flow
 
 interface ExpenseRepository {
-    suspend fun addExpense(groupId: String, expense: Expense): Result<Unit>
+    suspend fun addExpense(groupId: String, description: String, amount: Double): Result<Unit>
     suspend fun getExpensesByGroup(groupId: String): Flow<List<Expense>>
     suspend fun settleExpense(expenseId: String, userId: String): Result<Unit>
     suspend fun getExpensesByUser(userId: String): Flow<List<Expense>>
